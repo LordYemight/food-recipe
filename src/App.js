8afaaec0f9a4aff6545516e680553b627f14b153
Okunola Olayemi <Okunola.O@bullseye.local>
2023-05-26 14:12:58 +0100

    setup gh-pages

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Home from './pages/Home';
+import Pages from './pages/Pages';
+function App() {
+  return (
+    <div>
+      
+      <Home/>
+      <Pages/>
+    </div>
+    
+  );
+};
+
+export default App;
