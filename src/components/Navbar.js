3e14215c5b96bec25560f6228b9ea364e224f30c
IhinrereJesu <125207252+IhinrereJesu@users.noreply.github.com>
2023-05-27 08:41:47 -0700

    another commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 export default function Navbar(){
     return (
-        <div className="navbar">
-            <a href="#">F<span>oo</span>diesHub</a>
+        <div className="navbar container">
+            <a href="#" className="logo">F<span>oo</span>diesHub</a>
             <div className="nav-links">
                 <a href="#">Home</a>
                 <a href="#">Recipes</a>
